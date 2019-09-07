ee2d2d68f628b2bcd81bdf9cb0a4526b374df18f
MAC <>
2019-09-07 21:52:47 +0200

    Fix keyboard show and move textfield

--- OpenT/ViewController.swift
+++ OpenT/ViewController.swift
@@ -38,7 +38,57 @@ class ViewController: UIViewController, UITextFieldDelegate {
         
         nickOne.delegate = self
         nickTwo.delegate = self
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(ViewController.keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(ViewController.keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+        
     }
+    
+    @objc func keyboardWillShow(notification: NSNotification) {
+        if ((notification.userInfo?[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue) != nil {
+            if self.view.frame.origin.y == 0 {
+                self.view.frame.origin.y -= 60 //keyboardSize.height   //can adjust as keyboardSize.height-(any number 30 or 40)
+            }
+        }
+    }
+    
+    @objc func keyboardWillHide(notification: NSNotification) {
+        if self.view.frame.origin.y != 0 {
+            self.view.frame.origin.y = 0
+        }
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        nickOne.resignFirstResponder()
+        nickTwo.resignFirstResponder()
+        return true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        removeKeyboardObserver()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        addKeyboardChangeFrameObserver(willShow: { [weak self](height) in
+            //Update constraints here
+            self?.view.setNeedsUpdateConstraints()
+            }, willHide: { [weak self](height) in
+                //Reset constraints here
+                self?.view.setNeedsUpdateConstraints()
+        })
+    }
+    
+    
+    
+    
+    
+    
+    
+    
 }
 
 extension UIViewController {
@@ -52,5 +102,55 @@ extension UIViewController {
     @objc func dismissKeyboard()
     {
         view.endEditing(true)
+    }
+}
+//MARK: - Observers
+extension UIViewController {
+    
+    func addObserverForNotification(_ notificationName: Notification.Name, actionBlock: @escaping (Notification) -> Void) {
+        NotificationCenter.default.addObserver(forName: notificationName, object: nil, queue: OperationQueue.main, using: actionBlock)
+    }
+    
+    func removeObserver(_ observer: AnyObject, notificationName: Notification.Name) {
+        NotificationCenter.default.removeObserver(observer, name: notificationName, object: nil)
+    }
+}
+
+
+
+
+//MARK: - Keyboard handling
+extension UIViewController {
+    
+    typealias KeyboardHeightClosure = (CGFloat) -> ()
+    
+    func addKeyboardChangeFrameObserver(willShow willShowClosure: KeyboardHeightClosure?,
+                                        willHide willHideClosure: KeyboardHeightClosure?) {
+        NotificationCenter.default.addObserver(forName: UIResponder.keyboardWillChangeFrameNotification,
+                                               object: nil, queue: OperationQueue.main, using: { [weak self](notification) in
+                                                if let userInfo = notification.userInfo,
+                                                    let frame = (userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue,
+                                                    let duration = userInfo[UIResponder.keyboardAnimationDurationUserInfoKey] as? Double,
+                                                    let c = userInfo[UIResponder.keyboardAnimationCurveUserInfoKey] as? UInt,
+                                                    let kFrame = self?.view.convert(frame, from: nil),
+                                                    let kBounds = self?.view.bounds {
+                                                    
+                                                    let animationType = UIView.AnimationOptions(rawValue: c)
+                                                    let kHeight = kFrame.size.height
+                                                    UIView.animate(withDuration: duration, delay: 0, options: animationType, animations: {
+                                                        if kBounds.intersects(kFrame) { // keyboard will be shown
+                                                            willShowClosure?(kHeight)
+                                                        } else { // keyboard will be hidden
+                                                            willHideClosure?(kHeight)
+                                                        }
+                                                    }, completion: nil)
+                                                } else {
+                                                    print("-->Invalid conditions for UIKeyboardWillChangeFrameNotification")
+                                                }
+        })
+    }
+    
+    func removeKeyboardObserver() {
+        removeObserver(self, notificationName: UIResponder.keyboardWillChangeFrameNotification)
     }
 }
